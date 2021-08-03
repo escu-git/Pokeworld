d93d0db27230907af65bfaff8e6931008f2ba948
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-08-03 02:21:15 -0300

    ClickHandler added on pokeTypes

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import '../../Styles/styles.css'
 import LoginIcon from '../Login/LoginIcon';
-import PokemonTypes from '../Main/PokemonTypes/PokemonTypes';
 import PokeLogo from './Logo/PokeLogo';
 
 
 const NavBar = () => {
     return (
         <div className='navBar'>
+            <LoginIcon/>
         <NavLink to='/'>
             <PokeLogo/>
         </NavLink>
-            <LoginIcon/>
             <NavLink to='/all'>
             <button>TODOS</button>
             </NavLink>
